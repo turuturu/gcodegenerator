b2a786af4826427981fd5b4d37cb68f1cddb3087
ttsurumi <t.tsurumi@gmail.com>
2013-10-07 10:20:26 +0900

    remove gomi

--- rmagicksample.rb
+++
@@ -1,9 +0,0 @@
-require 'RMagick'
-include Magick
-
-#original = ImageList.new("images/sky-tate-04.jpg")
-cat = ImageList.new("Cheetah.jpg")
-thumb = cat.resize_to_fit(120,120)
-thumb.write "hoge.jpg"
-
-exit
